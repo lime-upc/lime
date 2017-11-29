5bc03e6f34645e9c3e2c14b2fee919fd8e0b9ba3
Luca Guida <luca.guida@est.fib.upc.edu>
2017-11-29 15:42:23 +0100

    Real Time Processor: RDD forEachPartition optimization implemented > significantly better performance on stream processing

--- backend/real-time-processor/src/main/java/org/lime/Main.java
+++ backend/real-time-processor/src/main/java/org/lime/Main.java
@@ -25,7 +25,6 @@ import org.elasticsearch.common.transport.TransportAddress;
 import org.elasticsearch.transport.client.PreBuiltTransportClient;
 
 import java.io.ByteArrayOutputStream;
-import java.io.IOException;
 import java.net.InetAddress;
 import java.sql.Timestamp;
 import java.text.DateFormat;
@@ -36,7 +35,6 @@ import java.time.Period;
 import java.time.ZoneId;
 import java.util.*;
 import java.util.concurrent.ExecutionException;
-import java.util.concurrent.TimeUnit;
 
 import static com.mongodb.client.model.Filters.eq;
 import static org.elasticsearch.common.xcontent.XContentFactory.jsonBuilder;
@@ -64,15 +62,11 @@ public class Main {
 
         directKafkaStream.foreachRDD(rdd ->{
 
-            rdd.foreach(avroRecord -> {
+            rdd.foreachPartition(partitionOfRecords -> {
 
-                //We receive pairs key-value, where the value is avro-serialized data.
-                byte[] encodedAvroData = avroRecord._2;
-                //We need to deserialize data to access the object
-                LocationType t = deserialize(encodedAvroData);
-                String email = t.getEmail().toString();
+                //MONGODB AND ELASTICSEARCH CONNECTIONS SETUP
 
-                // Creating a Mongo client for each RDD (as suggested here to avoid serialization problems: http://spark.apache.org/docs/latest/streaming-programming-guide.html#design-patterns-for-using-foreachrdd)
+                // Implemented best practice: Creating a Mongo client for each RDD partition (as suggested here to avoid serialization problems and have better performance: http://spark.apache.org/docs/latest/streaming-programming-guide.html#design-patterns-for-using-foreachrdd)
                 MongoClient mongo = new MongoClient ("localhost:27017");
 
                 // Accessing the database and the collections
@@ -80,14 +74,67 @@ public class Main {
                 MongoCollection<Document> usersCollection = database.getCollection("users");
                 MongoCollection<Document> spatialDBCollection = database.getCollection("spatialDB");
 
-                // Retrieving information about a user from mongoDB given his email address
-                Document userDoc = usersCollection.find(eq("email", email)).first();
-                Integer age = calculateAge(userDoc.get("date_of_birth").toString(), LocalDate.now());
+                // Creating the ElasticSearch Transport client
+                TransportClient client = new PreBuiltTransportClient(Settings.EMPTY)
+                        .addTransportAddress(new TransportAddress(InetAddress.getByName("localhost"), 9300));
 
-                //Save decorated user information in ElasticSearch index
-                saveInElasticSearch(t, userDoc);
+                // PROCESS EACH AVRO MESSAGE IN THE KAFKA FLOW
+                while (partitionOfRecords.hasNext()) {
 
-                System.out.println("Data about user "+ email +" retrieved from mongoDB and added to ElasticSearch DB!");
+                    //We receive pairs key-value, where the value is avro-serialized data.
+                    byte[] encodedAvroData = partitionOfRecords.next()._2;
+                    //We need to deserialize data to access the object
+                    LocationType t = deserialize(encodedAvroData);
+
+                    String email = t.getEmail().toString();
+                    Double lat = Double.parseDouble(t.getLat().toString());
+                    Double lon = Double.parseDouble(t.getLong$().toString());
+                    // Converting lat-lon coordinates into Military Grid Reference System coordinates
+                    CoordinateConversion coordConverter = new CoordinateConversion();
+                    String MGRScoord1 = coordConverter.latLon2MGRUTM(lat, lon); // MGRS coordinates with 1 meter precision
+                    String MGRScoord10 = MGRScoord1.substring(0,MGRScoord1.length()-2); // MGRS coordinates with 10 meters precision
+
+                    // Retrieving information about a user from mongoDB given his email address
+                    Document userDoc = usersCollection.find(eq("email", email)).first();
+                    Integer age = calculateAge(userDoc.get("date_of_birth").toString(), LocalDate.now());
+
+                    //inserting (if not existing already) or updating loc object into locations ElasticSearch index
+                    IndexRequest indexRequest = new IndexRequest("locations", "loc", userDoc.get("_id").toString())
+                            .source(jsonBuilder()
+                                    .startObject()
+                                    .field("MGRS_coord", MGRScoord10)
+                                    .field("lat", lat)
+                                    .field("long", lon)
+                                    .field("age", age)
+                                    .field("gender", userDoc.get("gender"))
+                                    .field("last_update_timestamp", new Timestamp(System.currentTimeMillis()))
+                                    .endObject());
+                    UpdateRequest updateRequest = new UpdateRequest("locations", "loc", userDoc.get("_id").toString())
+                            .doc(jsonBuilder()
+                                    .startObject()
+                                    .field("MGRS_coord", MGRScoord10)
+                                    .field("lat", lat)
+                                    .field("long", lon)
+                                    .field("last_update_timestamp", new Timestamp(System.currentTimeMillis()))
+                                    .endObject())
+                            .upsert(indexRequest);
+
+                    try {
+                        client.update(updateRequest).get();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    } catch (ExecutionException e) {
+                        e.printStackTrace();
+                    }
+
+                    System.out.println(">>> Data about user "+ email +" successfully retrieved from mongoDB and added to ElasticSearch index!");
+
+
+                }
+
+                // CLOSE MONGODB AND ELASTICSEARCH CONNECTIONS
+                //Close ES Transport Client >> Note: this adds an overhead in terms of time, but it is required to avoid that ES crashes
+                client.close();
 
                 //Closing the connection with mongoDB
                 mongo.close();
@@ -95,75 +142,8 @@ public class Main {
             });
         });
 
-
         jsc.start();
         jsc.awaitTermination();
-
-    }
-
-
-    /**
-     * Use this method to save current location data in the Elastic Search in-memory DB.
-     */
-
-    public static void saveInElasticSearch(LocationType t, Document doc) throws IOException {
-
-        // Creating the ElasticSearch Transport client
-        Settings settings = Settings.builder()
-                .put("client.transport.ping_timeout", 5, TimeUnit.SECONDS).build();
-        TransportClient client = new PreBuiltTransportClient(settings)
-                .addTransportAddress(new TransportAddress(InetAddress.getByName("localhost"), 9300));
-
-        Double lat = Double.parseDouble(t.getLat().toString());
-        Double lon = Double.parseDouble(t.getLong$().toString());
-
-        // Converting lat-lon coordinates into Military Grid Reference System coordinates
-        CoordinateConversion coordConverter = new CoordinateConversion();
-        String MGRScoord1 = coordConverter.latLon2MGRUTM(lat, lon); // MGRS coordinates with 1 meter precision
-        String MGRScoord10 = MGRScoord1.substring(0,MGRScoord1.length()-2); // MGRS coordinates with 10 meters precision
-        Integer age = calculateAge(doc.get("date_of_birth").toString(), LocalDate.now());
-
-        //inserting (if not existing already) or updating loc object into locations ElasticSearch index
-        IndexRequest indexRequest = new IndexRequest("locations", "loc", doc.get("_id").toString())
-                .source(jsonBuilder()
-                        .startObject()
-                        .field("MGRS_coord", MGRScoord10)
-                        .field("lat", lat)
-                        .field("long", lon)
-                        .field("age", age)
-                        .field("gender", doc.get("gender"))
-                        .field("last_update_timestamp", new Timestamp(System.currentTimeMillis()))
-                        .endObject());
-        UpdateRequest updateRequest = new UpdateRequest("locations", "loc", doc.get("_id").toString())
-                .doc(jsonBuilder()
-                        .startObject()
-                        .field("MGRS_coord", MGRScoord10)
-                        .field("lat", lat)
-                        .field("long", lon)
-                        .field("last_update_timestamp", new Timestamp(System.currentTimeMillis()))
-                        .endObject())
-                .upsert(indexRequest);
-
-        try {
-            client.update(updateRequest).get();
-
-            String email = t.getEmail().toString();
-            System.out.println("UPDATED: email=" + email + ", gender=" + doc.get("gender") + ", date_of_birth=" + doc.get("date_of_birth") + ", age=" + age + ", lat=" + lat + ", long=" + lon);
-
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        } catch (ExecutionException e) {
-            e.printStackTrace();
-        }
-
-/*
-        // MatchAll on the whole cluster with all default options
-        SearchResponse responseSearchAll = client.prepareSearch().get();
-        System.out.println("Search ALL: \n"+responseSearchAll);
-*/
-        //Close ES Transport Client >> Note: it adds a significant overhead in terms of time
-        client.close();
-
     }
 
 
@@ -177,7 +157,6 @@ public class Main {
 
         BinaryDecoder decoder = DecoderFactory.get().binaryDecoder(bytes, null);
         return datumReader.read(null,decoder);
-
     }
 
 
@@ -194,8 +173,6 @@ public class Main {
         encoder.flush();
         out.close();
         return out.toByteArray();
-
-
     }
 
     /**
