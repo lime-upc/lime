211380fef109b0f8914ca5273de19dbaf17cdb2a
Luca Guida <luca.guida@est.fib.upc.edu>
2017-12-01 16:16:28 +0100

    RealTimeES-API: some progress on the JS script

--- backend/real-time-ES-API/README.md
+++ backend/real-time-ES-API/README.md
@@ -1,3 +1,7 @@
 #Install Elastic Search  API in Node via NPM:
 npm install --save elasticsearch
+npm install bodybuilder --save
+
+#https://github.com/danpaz/bodybuilder
+#http://bodybuilder.js.org/docs/
 
--- backend/real-time-ES-API/script.js
+++ backend/real-time-ES-API/script.js
@@ -4,13 +4,19 @@ var bodybuilder = require('bodybuilder')
 
 var client = new elasticsearch.Client({
   host: 'localhost:9200',
-  log: 'trace'
+  //log: 'trace'
 });
 
 
 //var body = bodybuilder().query('match', 'MGRS_coord', '37NGC65272411').build()
 
-var body = bodybuilder().aggregation('terms', 'MGRS_coord').build()
+var body = bodybuilder()
+//.query('match', 'gender', 'male')
+//.query('match', 'gender', 'female')
+//.query('range', 'age', {gt: 25})
+//.query('range', 'age', {lt: 30})
+.aggregation('terms', 'MGRS_coord')
+.build()
 
 client.search({
   index: 'locations',
@@ -18,11 +24,18 @@ client.search({
   body: body
 }).then(function (resp) {
     var hits = resp.hits.hits;
-    console.trace(hits);
+    console.log(hits)
+
+	var aggregationsArray = resp.aggregations.agg_terms_MGRS_coord.buckets;
+    for (var i = 0; i < aggregationsArray.length; i++) {
+    	var key = aggregationsArray[i].key;
+    	var doc_count = aggregationsArray[i].doc_count;
+    	console.log(key + ": " + doc_count);
+	}
+
 }, function (err) {
     console.trace(err.message);
 });
-
 
 
 
